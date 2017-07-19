b20e2b562efd8e2619812fb754f463c8e66c3d3d
je7ov <jeffrey.ovalles@gmail.com>
2017-07-19 03:12:06 -0400

    Added top navigation buttons

--- landing_page/script.js
+++ landing_page/script.js
@@ -1,14 +1,67 @@
-const projects = document.querySelector("#projects").querySelectorAll(".project-item");
+// Get references to elements needed
+const projectsDiv = document.querySelector("#projects");
+const projects = projectsDiv.querySelectorAll(".project-item");
+const topNav = document.querySelector(".top-nav");
 
+// Initialize variables
+let topFixed = false;
+let topNavPixels = topNav.offsetTop;
+
+// General animate function with animation frame requests
+function animate({duration, draw, timing}) {
+	let start = performance.now();
+
+	return requestAnimationFrame(function animate(time) {
+		let timeFraction = (time - start) / duration;
+		if (timeFraction > 1) timeFraction = 1;
+
+		let progress = timing(timeFraction);
+		
+		draw(progress);
+
+		if (timeFraction < 1) {
+			requestAnimationFrame(animate);
+		}
+	});
+}
+
+// Handle functionality of top navigation buttons fixing to top
+function checkTopNav() {
+	console.log(window.getComputedStyle(topNav.children[0], null).height);
+
+	const topPixels = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+	if (topFixed) {
+		if (topPixels - 2 < topNavPixels) {
+			topNav.classList.toggle("fixed-top");
+			projectsDiv.classList.toggle("top-nav-padding");
+			topFixed = false;
+		}
+	} else {
+		let topNavPixels = topNav.offsetTop;
+		if (topPixels - 2 >= topNavPixels) {
+			topNav.classList.toggle("fixed-top");
+			projectsDiv.classList.toggle("top-nav-padding");
+			topFixed = true;
+		}
+	}
+}
+
+// Add scroll listener
+function scrollListener() {
+	checkTopNav();
+}
+window.addEventListener("scroll", scrollListener);
+
+// Flip every other project item listed
 for (const [index, project] of projects.entries()) {
 	const divs = project.children;
-	
+	let colCount = 0;
 	if (index%2 == 1) {
 		for (let i=0; i < divs.length; i++) {
 			const divClass = divs[i].classList[0];
 			const colType = divClass.substring(0, 7);
 			const colNum = divClass.substring(7, divClass.length);
-			const pushing = i < divs.length/2;
+			const pushing = i === 0;
 			const newClass = `${colType}${pushing ? "push-" : "pull-"}${12 - colNum}`;
 
 			divs[i].classList.add(newClass);
