b42ee62c0745358f075e9f016cca733fdff4ed74
je7ov <jeffrey.ovalles@gmail.com>
2017-07-19 22:55:07 -0400

    Added active button style for section currently looked at

--- script.js
+++ script.js
@@ -2,11 +2,17 @@
 const topNavPadding = document.querySelector("#top-nav-padding");
 const projects = document.querySelector("#projects").querySelectorAll(".project-item");
 const topNav = document.querySelector(".top-nav");
+const topNavButtons = topNav.querySelectorAll("button");
+const projectsAnchor = document.querySelector("#projects-anchor");
+const aboutMeAnchor = document.querySelector("#about-me-anchor");
+const contactsAnchor = document.querySelector("#contacts-anchor");
 
 // Initialize variables
 let topFixed = false;
 let topNavPixels = topNav.offsetTop;
 let scrollAnimationFrame = null;
+let activeButton = -1;
+
 
 // Flip every other project item listed
 for (const [index, project] of projects.entries()) {
@@ -42,10 +48,10 @@ function quad(progress) {
 }
 
 // General animate function with animation frame requests
-function animate({duration, draw, timing}) {
+function animate({duration, draw, timing, ref}) {
 	let start = 1.0 * performance.now();
 
-	return requestAnimationFrame(function animate(time) {
+	ref = requestAnimationFrame(function animate(time) {
 		let timeFraction = (time - start) / duration;
 		if (timeFraction > 1) timeFraction = 1;
 		else if (timeFraction < 0) timeFraction = 0;
@@ -55,7 +61,7 @@ function animate({duration, draw, timing}) {
 		draw(progress);
 
 		if (timeFraction < 1) {
-			requestAnimationFrame(animate);
+			ref = requestAnimationFrame(animate);
 		}
 	});
 }
@@ -76,21 +82,21 @@ function scrollToID(id) {
 		return;
 	}
 
-	let pixelsToScroll = anchor.offsetTop - topPixels - anchorHeight + 60;
+	const pixelsToScroll = anchor.offsetTop - topPixels - anchorHeight + 60;
 	const animDuration = Math.abs(pixelsToScroll) / 4.5;
 
-	scrollAnimationFrame = animate({
+	animate({
 		duration: animDuration,
 		timing: makeEaseInOut(quad),
 		draw: (progress) => {
-
 			window.scrollTo(0, (pixelsToScroll * progress) + topPixels);
-		}
+		},
+		ref: scrollAnimationFrame
 	})
 }
 
 // Handle functionality of top navigation buttons fixing to top
-function checkTopNav() {
+function handleTopNav() {
 	const topPixels = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
 	if (topFixed) {
 		if (topPixels < topNavPixels) {
@@ -110,9 +116,70 @@ function checkTopNav() {
 	}
 }
 
+function handleTopNavButtons() {
+	const topPixels = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+	let noChange = false;
+
+	// Check for first button
+	const projectsAnchorStyle = window.getComputedStyle(projectsAnchor, null);
+	let projectsAnchorHeight = projectsAnchorStyle.height;
+	projectsAnchorHeight = Number(projectsAnchorHeight.substring(0, projectsAnchorHeight.indexOf("px")));
+	const projectsAnchorScroll = projectsAnchor.offsetTop - projectsAnchorHeight + 60;
+
+	if (projectsAnchorScroll <= topPixels) {
+		if (activeButton === 0) noChange = true;
+		else activeButton = 0;
+
+		// Check for second button
+		const aboutMeAnchorStyle = window.getComputedStyle(aboutMeAnchor, null);
+		let aboutMeAnchorHeight = aboutMeAnchorStyle.height;
+		aboutMeAnchorHeight = Number(aboutMeAnchorHeight.substring(0, aboutMeAnchorHeight.indexOf("px")));
+		const aboutMeAnchorScroll = aboutMeAnchor.offsetTop - aboutMeAnchorHeight + 60;
+		
+		if (aboutMeAnchorScroll <= topPixels) {
+			if (activeButton === 1) noChange = true;
+			else {
+				activeButton = 1;
+				noChange = false;
+			}
+
+			// Check for third button
+			// const contactsAnchorStyle = window.getComputedStyle(contactsAnchor, null);
+			// let contactsAnchorHeight = contactsAnchorStyle.height;
+			// contactsAnchorHeight = Number(contactsAnchorHeight.substring(0, contactsAnchorHeight.indexOf("px")));
+			// const contactsAnchorScroll = contactsAnchor.offsetTop - contactsAnchorHeight + 60;
+			const body = document.body
+	    const html = document.documentElement;
+
+			const height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+
+			// if (contactsAnchorScroll <= topPixels) {
+			if (topPixels + window.innerHeight >= height) {
+				if (activeButton === 2) noChange = true;
+				else {
+					activeButton = 2;
+					noChange = false;
+				}
+			}
+		}
+	} else {
+		activeButton = -1;
+	}
+
+	if (!noChange) {
+		for (const [index, button] of topNavButtons.entries()) {
+			if (index === activeButton) {
+				button.classList.add("active");
+			} else {
+				button.classList.remove("active");
+			}
+		}
+	}
+}
+
 // Add scroll listener
 function scrollListener() {
-	window.cancelAnimationFrame(scrollAnimationFrame);
-	checkTopNav();
+	handleTopNav();
+	handleTopNavButtons();
 }
 window.addEventListener("scroll", scrollListener);
