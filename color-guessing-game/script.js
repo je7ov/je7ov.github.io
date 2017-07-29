212ee052cf99e4f7177e14d9e3a7869678284839
je7ov <jeffrey.ovalles@gmail.com>
2017-07-28 20:26:37 -0400

    Added more animations, score, difficulties to color game. Cleaned up code

--- color-guessing-game/script.js
+++ color-guessing-game/script.js
@@ -1,115 +1,170 @@
 // variables
 let colors = [];
 let squares = [];
-let correct, wrongCount, done;
+let correct, wrongCount, done, lose;
+let score = 0;
 let difficulty = 0;
+let boardRows = 1;
 
 // constants
 const winResponses = ['Good job!', 'Nice job!', 'Awesome!', 'Winner!'];
 const loseResponses = ['Nice try', 'Try again', 'Better luck next time', 'Too bad'];
 
 // DOM references
-const squareDivs = document.querySelectorAll('.square');
-const squareBorders = document.querySelectorAll('.square-border');
-const rgbDisplay = document.querySelector('span');
+let squareDivs = document.querySelectorAll('.square');
+let squareBorders = document.querySelectorAll('.square-border');
+const board = document.querySelector('#board');
+const resultDisplay = document.querySelector('h5');
 const resetButton = document.querySelector('#reset');
-const easyButton = document.querySelector('#easy');
-const hardButton = document.querySelector('#hard');
+const difficultyButtons = document.querySelector('#difficulty-container').querySelectorAll('h4');
+const rgbDisplay = document.querySelector('span');
 const title = document.querySelector('#title');
-const secondRow = document.querySelector('.row:nth-of-type(2)');
-const results = document.querySelector('h5');
+const scoreDisplay = document.querySelector('#score');
 
+// Start game
+startup();
 initialize();
 
-// Add listeners to squares
-for (const [index, square] of squareDivs.entries()) {
-	square.addEventListener('click', () => {
-		if (!done) {
-			if (index === correct) {
-				// squareBorders[index].classList.add('correct');
-				squareBorders[index].classList.add('lower');
-				const randResponse = Math.floor(winResponses.length * Math.random());
-				results.innerHTML = winResponses[randResponse];
-				endGame();
-			} else {
-				squareBorders[index].classList.add('lower');
-				squareBorders[index].classList.add('disappear');
-				wrongCount++;
-				if (wrongCount >= colors.length - 1) {
-					const randResponse = Math.floor(loseResponses.length * Math.random());
-					results.innerHTML = loseResponses[randResponse];
-					endGame();
-				};
+// ------- GAME LOGIC SECTION ------- //
+function startup() {
+	// Add listeners to buttons
+	for (const [index, button] of difficultyButtons.entries()) {
+		button.addEventListener('click', () => {
+			if (difficulty !== difficultyButtons.length - index -1) {
+				difficulty = difficultyButtons.length - index - 1;
+				setActiveDifficulty();
+				reset();
 			}
-		}
-	});
+		});
+	}
 
-	squareBorders[index].addEventListener('mouseenter', () => {
-		if (!done) squareBorders[index].classList.add('hovered');
-	})
+	scoreDisplay.innerHTML = '0';
 
-	squareBorders[index].addEventListener('mouseleave', () => {
-		if (!done) squareBorders[index].classList.remove('hovered');
-	})
+	setActiveDifficulty();
+
+	resetButton.addEventListener('click', reset);
 }
 
-// Add listeners to difficulty buttons
-easyButton.addEventListener('click', () => {
-	if (difficulty === 1) {
-		difficulty = 0;
-		easyButton.classList.add('active');
-		hardButton.classList.remove('active');
-		reset();
-	}
-});
-hardButton.addEventListener('click', () => {
-	if (difficulty === 0) {
-		difficulty = 1;
-		hardButton.classList.add('active');
-		easyButton.classList.remove('active');
-		reset();
-	}
-})
-
-// Add listener to reset button
-resetButton.addEventListener('click', reset);
-
-// Initialize game
 function initialize() {
-	difficulty === 1 ? getRandomColors(6) : getRandomColors(3);
+	const numColors = (difficulty + 1) * 3;
+	colors = getRandomColors(numColors);
 	correct = Math.floor(colors.length * Math.random());
 	wrongCount = 0;
 	done = false;
+	lose = false;
 
-	if (difficulty === 0) {
-		easyButton.classList.add('active');
-		secondRow.classList.remove('reappear');
-		secondRow.classList.add('disappear');
-	} else {
-		hardButton.classList.add('active');
-		secondRow.classList.remove('disappear');
-		secondRow.classList.add('reappear');
-	}
+	setupBoard();
 
+	// Make square objects with the random colors
 	squares = [];
-	for(const [index, color] of colors.entries()) {
-		squares[index] = new squareObj(color)
+	for (const [index, color] of colors.entries()) {
+		squares[index] = new squareObj(color);
 	}
 
-	// Initialize squares with colors
-	for (let index=0; index < colors.length; index++) {
-		squareDivs[index].style.backgroundColor = squares[index].color;
-		squareBorders[index].style.background = squares[index].borderColor;
+	// Display colors of squares
+	const duration = 750;
+	for (const [index, square] of squareDivs.entries()) {
+		const squareColor = getBackgroundColor(square);
+		const squareBorderColor = getBackgroundColor(squareBorders[index]);
+		fadeColor(square, 'background-color', squareColor, colors[index], duration);
+		fadeColor(squareBorders[index], 'background-color', squareBorderColor, squares[index].borderColorRaw, duration);
 	}
 
 	rgbDisplay.innerHTML = `(<span class='red'>${colors[correct].r}</span>,
 													<span class='green'>${colors[correct].g}</span>,
 													<span class='blue'>${colors[correct].b}</span>)`;
-
-	title.style.backgroundColor = '#232323';
+    const titleColor = getBackgroundColor(title);
+	fadeColor(title, 'background-color', titleColor, {r: 35, g: 35, b: 35}, 650);
 }
 
-// Get a random rgb color
+function getBackgroundColor(element) {
+	let bgColor = window.getComputedStyle(element).backgroundColor;
+	bgColor = bgColor.substring(4, bgColor.length - 1).replace(/ /g, '').split(',');
+	bgColor = {r: bgColor[0], g: bgColor[1], b: bgColor[2]}
+	return bgColor;
+}
+
+// Set up the board
+function setupBoard() {
+	if (boardRows !== difficulty) {
+		board.innerHTML = "";
+		boardRows = difficulty;
+		for (let i = 0; i <= difficulty; i++) {
+			const rowDiv = document.createElement('div');
+			rowDiv.className = 'row';
+			for (let i=0; i < 3; i++) {
+				const squareBorder = document.createElement('div');
+				squareBorder.className = 'square-border';
+				const squareInner = document.createElement('div');
+				squareInner.className = 'square';
+				squareBorder.appendChild(squareInner);
+				rowDiv.appendChild(squareBorder);
+			}
+			board.appendChild(rowDiv);
+		}
+
+		squareDivs = document.querySelectorAll('.square');
+		squareBorders = document.querySelectorAll('.square-border');
+		addBoardListeners();
+	}
+}
+
+// Add listeners to squares in the board
+function addBoardListeners() {
+	for (const [index, square] of squareBorders.entries()) {
+		square.addEventListener('click', () => {
+			if (!done && !square.classList.contains('disappear')) {
+				if (index === correct) {
+					correctGuess(square);
+				} else {
+					wrongGuess(square);
+				}
+			}
+		});
+
+		square.addEventListener('mouseenter', () => {
+			if (!done) square.classList.add('hovered');
+		});
+
+		square.addEventListener('mouseleave', () => {
+			if (!done) square.classList.remove('hovered');
+		})
+	}
+}
+
+function correctGuess(square) {
+	square.classList.add('lower');
+	const randResponse = Math.floor(winResponses.length * Math.random());
+	resultDisplay.innerHTML = winResponses[randResponse];
+	score++;
+	setTimeout(reset, 1500);
+	endGame();
+}
+
+function wrongGuess(square) {
+	square.classList.add('lower', 'disappear');
+	wrongCount++;
+	// TODO: Adjust loss conditions
+	if (wrongCount >= Math.sqrt(colors.length)) {
+		const randResponse = Math.floor(loseResponses.length * Math.random());
+		resultDisplay.innerHTML = loseResponses[randResponse];
+		score = 0;
+		lose = true;
+		endGame();
+	}
+}
+
+// Set current difficulty button to active
+function setActiveDifficulty() {
+	for (const [index, button] of difficultyButtons.entries()) {
+		if (difficulty === difficultyButtons.length - index - 1) button.classList.add('active');
+		else button.classList.remove('active');
+	}
+	score = 0;
+	scoreDisplay.innerHTML = '0';
+}
+
+// Get random rgb color
 function randomColor() {
 	const r = Math.round(255 * Math.random());
 	const g = Math.round(255 * Math.random());
@@ -117,34 +172,66 @@ function randomColor() {
 	return {r, g, b};
 }
 
-// Get a random color for each square
+// Get array of random colors
 function getRandomColors(num) {
-	colors = [];
-	for (let i=0; i < num; i++) {
-		colors[i] = randomColor();
+	colArr = [];
+	for (let i=0; i < num; i++) colArr[i] = randomColor();
+	return colArr;
+}
+
+// End game
+function endGame() {
+	setEndGameColors();
+	const red = document.querySelector('.red');
+	const green = document.querySelector('.green');
+	const blue = document.querySelector('.blue');
+	red.classList.add('stroke');
+	green.classList.add('stroke');
+	blue.classList.add('stroke');
+	if (!lose) {
+		scoreDisplay.innerHTML = `${score}`;
+	}
+	done = true;
+}
+
+// Change title background and all squares to correct color
+function setEndGameColors() {
+	const delay = 80;
+	let counter = 0;
+
+	const titleColor = getBackgroundColor(title);
+	fadeColor(title, 'background-color', titleColor, colors[correct], 500);
+	for (let index=0; index < colors.length; index++) {
+		squareBorders[index].classList.remove('hovered', 'lower');
+		if (index !== correct) {
+			counter++;
+			if (squareBorders[index].classList.contains('disappear')) {
+				squareBorders[index].classList.remove('disappear');
+				squareBorders[index].classList.add('reappear');
+			}
+
+			const duration = 350;
+			setTimeout(() => {
+				fadeColor(squareDivs[index], 'background-color', colors[index], colors[correct], duration);
+				fadeColor(squareBorders[index], 'background-color', squares[index].borderColorRaw, squares[correct].borderColorRaw, duration);
+			}, delay * counter);
+		}
 	}
 }
 
-// Fade color property
-function fade(element, property, start, end, duration) {
-	animate({
-		duration,
-		timing: makeEaseInOut(quad),
-		draw: (progress) => {
-			let r = Math.round(lerp(start.r, end.r, progress));
-			let g = Math.round(lerp(start.g, end.g, progress));
-			let b = Math.round(lerp(start.b, end.b, progress));
-			let colorname = `rgb(${r}, ${g}, ${b})`;
-
-			element.style.setProperty(property, colorname);
-		}
-	})
+// Reset game
+function reset() {
+	if (lose) scoreDisplay.innerHTML = '0';
+	initialize();
+	for (square of squareBorders) {
+		square.classList.remove('lower', 'hovered', 'reappear', 'disappear');
+	}
+	resultDisplay.innerHTML = '';
 }
 
-// Linear interpolator
-function lerp(a, b, ratio) {
-	return (1 - ratio) * a + ratio * b;
-}
+// ------- END GAME LOGIC SECTION ------- //
+
+// ------- ANIMATION SECTION ------- //
 
 // General animate function with animation frame requests
 function animate({duration, draw, timing}) {
@@ -164,6 +251,32 @@ function animate({duration, draw, timing}) {
 	});
 }
 
+// Fade color property
+function fadeColor(element, property, start, end, duration) {
+	animate({
+		duration,
+		timing: makeEaseInOut(quad),
+		draw: (progress) => {
+			let r = Math.round(lerp(start.r, end.r, progress));
+			let g = Math.round(lerp(start.g, end.g, progress));
+			let b = Math.round(lerp(start.b, end.b, progress));
+			let colorname = `rgb(${r}, ${g}, ${b})`;
+
+			element.style.setProperty(property, colorname);
+		}
+	})
+}
+
+// Linear interpolator
+function lerp(a, b, ratio) {
+	return (1 - ratio) * a + ratio * b;
+}
+
+// Quadratic timing function
+function quad(progress) {
+	return Math.pow(progress, 2);
+}
+
 // Turn timing function into ease in and out
 function makeEaseInOut(timing, timeFraction) {
 	return function(timeFraction) {
@@ -175,50 +288,4 @@ function makeEaseInOut(timing, timeFraction) {
 	}
 }
 
-// Quadratic timing function
-function quad(progress) {
-	return Math.pow(progress, 2);
-}
-
-// End game
-function endGame() {
-	endGameSquares();
-	fade(title, 'background-color', {r: 35, g: 35, b: 35}, colors[correct], 500);
-	const red = document.querySelector('.red');
-	const green = document.querySelector('.green');
-	const blue = document.querySelector('.blue');
-	red.classList.add('stroke');
-	green.classList.add('stroke');
-	blue.classList.add('stroke');
-	done = true;
-}
-
-// Change all squares to correct color
-function endGameSquares() {
-	const delay = 80;
-	let counter = 0;
-	for (let index=0; index < colors.length; index++) {
-		squareBorders[index].classList.remove('hovered', 'lower');
-		if (index !== correct) {
-			counter++;
-			if (squareBorders[index].classList.contains('disappear')) {
-				squareBorders[index].classList.remove('disappear');
-				squareBorders[index].classList.add('reappear');
-			}
-
-			setTimeout(() => {
-				const duration = 250;
-				fade(squareDivs[index], 'background-color', colors[index], colors[correct], duration);
-				fade(squareBorders[index], 'background-color', squares[index].borderColorRaw, squares[correct].borderColorRaw, duration);
-			}, delay * counter);
-		}
-	}
-}
-
-function reset() {
-	initialize();
-	for (square of squareBorders) {
-		square.classList.remove('lower', 'hovered', 'reappear', 'disappear');
-	}
-	results.innerHTML = '';
-}
+// ------- END ANIMATION SECTION ------- //
