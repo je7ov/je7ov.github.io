21d51268ef4a6cadb5ff77dc2c392b193cfa6847
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-08-25 20:24:45 -0400

    Added react-native page rough draft

---
+++ react-native/script.js
@@ -0,0 +1,22 @@
+const sections = document.querySelectorAll(".section");
+
+function sectionListener() {
+
+}
+
+function toggleSectionContent(clickedIndex) {
+  for (const [index, section] of sections.entries()) {
+    const content = section.querySelector(".section-content");
+    if (index === clickedIndex) {
+      content.classList.toggle("show");
+    } else {
+      content.classList.remove("show");
+    }
+  }
+}
+
+for (const [index, section] of sections.entries()) {
+  section.querySelector(".section-text").addEventListener("click", () => {
+    toggleSectionContent(index)
+  });
+}
