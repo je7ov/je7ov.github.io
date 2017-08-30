148557706b5a936c49ca9672be2a5c45d436eb10
Jeffrey Ovalles <jeffrey.ovalles@techclub.nyc>
2017-08-30 15:57:24 -0400

    Update top navigation buttons

--- react-native/script.js
+++ react-native/script.js
@@ -1,4 +1,29 @@
 const sections = document.querySelectorAll(".section");
+const topNav = document.querySelector(".top-nav");
+const topNavPadding = document.querySelector("#top-nav-padding");
+
+let topFixed = false;
+topNavPixels = topNav.offsetTop;
+
+function handleTopNav() {
+  const topPixels = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  if (topFixed) {
+    if (topPixels < topNavPixels) {
+      topNav.classList.toggle("fixed-top");
+      topNavPadding.classList.toggle("top-nav-padding");
+      topNavPadding.classList.toggle("hidden");
+      topFixed = false;
+    }
+  } else {
+    let topNavPixels = topNav.offsetTop;
+    if (topPixels >= topNavPixels) {
+      topNav.classList.toggle("fixed-top");
+      topNavPadding.classList.toggle("top-nav-padding");
+      topNavPadding.classList.toggle("hidden");
+      topFixed = true;
+    }
+  }
+}
 
 function toggleSectionContent(clickedIndex) {
   for (const [index, section] of sections.entries()) {
@@ -16,3 +41,8 @@ for (const [index, section] of sections.entries()) {
     toggleSectionContent(index)
   });
 }
+
+function scrollListener() {
+  handleTopNav();
+}
+window.addEventListener("scroll", scrollListener);
